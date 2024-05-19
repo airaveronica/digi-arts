df3ad0eadf7b8e03e77c022768107bc56d5b4165
ciprianoaljhon <113755577+ciprianoaljhon@users.noreply.github.com>
2024-05-19 21:16:29 +0800

    update

--- data.js
+++ data.js
@@ -53,7 +53,7 @@ export const data = [
     logoURL: "GROUP2_MYSTERDISTRO_LOGO.PNG",
     members: [
       {
-        name: "Carlo", h
+        name: "Carlo",
         imgURL: "GROUP2_MYSTERDISTRO_FORMAL/Carlo.jpg",
       },
       {
