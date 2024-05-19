812052e90a60f256dfaace80650ee2bf522498d2
airaveronica <airaveronicavitocruz854@gmail.com>
2024-05-19 21:14:28 +0800

    Update data.js

--- data.js
+++ data.js
@@ -8,11 +8,11 @@ export const data = [
     id: "chiq_boutique",
     name: "Chiq Boutique",
     tagline: "From the lace, til’ the bows.",
-    mission:
-      "We aim to transform traditional beauty standards by offering innovative and environmentally-friendly accessory products that encourage the principles of reuse, reduction, and recycling. Enabling individuals to possess items that are not only visually appealing but also possess long-lasting quality.",
+    mission: 
+      "Chic Boutique aims to transform traditional beauty standards by offering innovative and environmentally-friendly accessory products that encourage the principles of reuse, reduction, and recycling. Enabling individuals to possess items that are not only visually appealing but also possess long-lasting quality. The company is committed to minimizing the disposal of unused clothing by repurposing it through innovative means. Chic Boutique promotes the adoption of mindful practices by offering a wide range of fashionable and environmentally-friendly accessories for individuals to wear and style.",
     vision:
-      "We imagine a world where beauty and all its diverse manifestations are embraced and celebrated, empowering individuals to confidently embrace their unique ideas with fashion accessories.",
-    coreValues: "",
+      "The vision of Chic Boutique is to envision a world where beauty, in all its various forms, is embraced and celebrated, empowering individuals to confidently embrace their distinct ideas through fashion accessories. The company's objective is to create a future in which accessories serve not only as functional items to be utilized and stored but also as unused items that can be utilized and reused. The organization aims to be acknowledged as a proponent of the three (3) R's, establish a platform for our creations, and develop groundbreaking products that are in line with the organization's mission and core values. Chic Boutique aims to demonstrate the connection between innovation and sustainability while also offering aesthetic appeal. We imagine a world where beauty and all its diverse manifestations are embraced and celebrated, empowering individuals to confidently embrace their unique ideas with fashion accessories.",
+    coreValues: "-Transparency: To increase transparency with the community and establish a strong corporate identity. Particularly given that the community plays a crucial role in constructing and molding the products we offer, with the aim of improving the environment and reducing waste. -Collaboration: to emphasize the recognition that certain tasks cannot be accomplished in isolation. Chic Boutique offers assistance or requests aid when one requires it. Collaboration is essential for ensuring proper execution of tasks. -﻿Innovation: To discover novel methods to decrease waste and mitigate environmental harm while simultaneously offering stylish products that prioritize sustainability. -Quality: to produce products that not only reduce waste but also exhibit exceptional quality and durability. -Creativity: To deliver innovative products that are visually appealing and showcase their elegant design. One of the company's primary objectives is to not only offer long-lasting and top-notch products but also to demonstrate their ingenuity.",
     logoURL: "GROUP5_CHICBOUTIQUE_LOGO.png",
     members: [
       {
@@ -47,9 +47,9 @@ export const data = [
     id: "myster_distro",
     name: "Myster Distro",
     tagline: "A Meadow in Your Own Home",
-    mission: "",
-    vision: "",
-    coreValues: "",
+    mission: "Our mission is to capture and re-introduce retro styles to the younger generation.",
+    vision: "To pride ourselves as the nation's choice in retro based products such as accessories, art and mystery packages.",
+    coreValues: "The company's philosophy is to retain retro aesthetics in our products with respect, creativity, innovation, and resourcefulness. The health and well-being of the company's workers are taken priority in order to benefit morale, motivation, teamwork, and productivity. Transparency, proper communication, and trust are also important to the company in business deals, and transactions, as well as for public opinion, in which engagement with the target customers is also important.",
     logoURL: "GROUP2_MYSTERDISTRO_LOGO.PNG",
     members: [
       {
@@ -82,12 +82,12 @@ export const data = [
 
   // copy paste for other groups
   {
-    id: "myster_distro", // company name lowercase separated by underscore (no space)
-    name: "Myster Distro", // company name
-    tagline: "A Meadow in Your Own Home",
-    mission: "",
-    vision: "",
-    coreValues: "",
+    id: "botanical_tavern", // company name lowercase separated by underscore (no space)
+    name: "Botanical Tavern", // company name
+    tagline: "Where Growth Flourishes, Creativity Blooms.",
+    mission: "The mission is to inspire and delight the community by offering a careful selection of botanical-inspired stickers and figurines that ignite the imagination and celebrate the beauty of nature. Through continuous innovation and collaboration, the company strives to cultivate a welcoming space where individuals can explore their love for art and nature, fostering connections and sparking joy. With integrity and respect for the planet, they aim to leave a positive impact on both the customers and the environment.",
+    vision: "At Botanical Tavern, the company envisions a vibrant community where creativity thrives. The vision is to cultivate a community where individuals can explore their love for art and nature through the collection of stickers and figurines. They aspire to cultivate a community—with a commitment to quality, and innovation. They strive to become a trusted destination where enthusiasts and collectors alike gather to celebrate the beauty of nature and art. Together, They paint a picture of a future where the Botanical Tavern is not just a brand, but a beacon—an emblem of boundless creativity, perpetual inspiration, and the enduring allure of the natural world",
+    coreValues: "The philosophy is rooted in integrity, creativity, and innovation. The company believes in creating a community where individuals can explore their love of art and nature, they aspire to create an environment that brings joy and imagination that could enrich the lives of the customers and community.",
     logoURL: "GROUP2_MYSTERDISTRO_LOGO.PNG", // logo file name (case sensitive)
     members: [
       //copy paste for other members
